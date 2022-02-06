fd378f5a42dd2a87c8e0fc477db6bab84fa75ace
Jack Garrard <speyejack@gmail.com>
2022-02-06 02:01:27 -0700

    Simplifed filtering method in game parameters

--- src/logic/params.rs
+++ src/logic/params.rs
@@ -16,22 +16,21 @@ pub struct GameParameters {
 impl Default for GameParameters {
     fn default() -> Self {
         let word_size = (5, 5);
+		let filter_words = |x: &str| {
+			Some(x)
+				.filter(|x| x.len() >= word_size.0 && x.len() <= word_size.1)
+				.map(|x| x.to_string())
+		};
+
+
         let answer_wordlist: Vec<String> = include_str!("../../words/answers.txt")
             .lines()
-            .filter_map(|x| {
-                Some(x)
-                    .filter(|x| x.len() >= word_size.0 && x.len() <= word_size.1)
-                    .map(|x| x.to_string())
-            })
+            .filter_map(filter_words)
             .collect();
 
         let guess_wordlist = include_str!("../../words/guesses.txt")
             .lines()
-            .filter_map(|x| {
-                Some(x)
-                    .filter(|x| x.len() > word_size.0 && x.len() < word_size.1)
-                    .map(|x| x.to_string())
-            })
+            .filter_map(filter_words)
             .chain(answer_wordlist.clone())
             .collect();
 
